c33ca79a82846e13ea96985cd378a137a2361012
asazin90143 <kent.escala143@gmail.com>
2026-02-22 18:11:17 +0800

    Basic Authentication with requests 2/2

--- Itroduction to APIs in Python/Basic Authentication with requests.py
+++ Itroduction to APIs in Python/Basic Authentication with requests.py
@@ -16,3 +16,25 @@ if (response.status_code == 200):
 # Check if the response status code is 401
 elif (response.status_code == 401):
   print('Authentication failed!')
+
+
+#Instructions 2/2
+
+# Create the correct `authentication` variable with your username and password.
+# Then pass the `authentication` variable to the `requests.get()` method using the correct argument.
+
+# Import the requests package
+import requests
+
+# Create the authentication tuple with the correct values for basic authentication
+authentication = ('john@doe.com', 'Warp_ExtrapolationsForfeited2')
+
+# Use the correct function argument to pass the authentication tuple to the API
+response = requests.get('http://localhost:3000/albums', auth = authentication)
+
+if(response.status_code == 200):
+    print("Success!")
+elif(response.status_code == 401):
+    print('Authentication failed')
+else:
+    print('Another error occurred')

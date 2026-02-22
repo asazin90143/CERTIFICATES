132cd76f9cbfc45c769e93748dce596b2b3c9eda
asazin90143 <kent.escala143@gmail.com>
2026-02-22 17:23:34 +0800

    Response codes and APIs 1/3

---
+++ Itroduction to APIs in Python/Response codes and APIs.py
@@ -0,0 +1,16 @@
+#Instructions 1/3
+# Check if the server responded successfully with the `200` status code.
+import requests
+response = requests.get('http://localhost:3000/lyrics')
+
+# Check the response status code
+if (response.status_code == 200):
+  print('The server responded succesfully!')
+
+#Instructions 2/3
+#Perform a request to the inexistent `/movies` path of the music catalog API.
+# Check if the server responded with a status code indicating the resource was not found, providing the appropriate numerical status code representing this.
+
+
+#Instructions 3/3
+#Check for response codes with a `200 OK` and `404 Not found` status code using the `requests.codes` lookup object.

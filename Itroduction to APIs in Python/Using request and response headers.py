e94e72115249b7da059b765f1c5a32669cd34f2a
asazin90143 <kent.escala143@gmail.com>
2026-02-22 17:38:01 +0800

    Using request and response headers 1/3

---
+++ Itroduction to APIs in Python/Using request and response headers.py
@@ -0,0 +1,15 @@
+#Instructions 1/3
+# Find out the content-type of the response by printing out the response `content-type` header.
+import requests
+response = requests.get('http://localhost:3000/lyrics')
+
+# Print the response content-type header
+print(response.headers['content-type'])
+
+
+#Instructions 2/3
+# Find out what content-types the server can respond with by printing out the response `accept` header.
+
+
+#Instructions 3/3
+# Add an `accept` header to the request so the server returns JSON formatted data, then print the response `text` attribute.

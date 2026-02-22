51f1392234c8c5f409ce8ffa54d66474233e9f62
asazin90143 <kent.escala143@gmail.com>
2026-02-22 12:57:14 +0800

    Constructing a URL with parameters 2/3

--- Itroduction to APIs in Python/Constructing a URL with parameters.py
+++ Itroduction to APIs in Python/Constructing a URL with parameters.py
@@ -13,3 +13,21 @@ response = requests.get('http://localhost:3000/lyrics/random')
 # Print the response text
 print(response.text)
 
+#Instructions 2/3
+
+#Let's now add a query parameter to only get lyrics by a specific artist.
+
+#Create a dictionary variable with one entry: the key `artist` with a value of `Deep Purple`.
+#Pass this dictionary to the `requests.get()` method as the `params` argument.
+
+# Import the requests package
+import requests
+
+# Create a dictionary variable with query params
+query_params = {'artist': 'Deep Purple'}
+
+# Pass the dictionary to the get() function
+response = requests.get('http://localhost:3000/lyrics/random', params=query_params)
+
+# Print the response text
+print(response.text)

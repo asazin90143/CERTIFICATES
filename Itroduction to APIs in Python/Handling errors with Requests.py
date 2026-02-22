20163583d0d84c6a6aff1e6e027a01eabb9c81eb
asazin90143 <kent.escala143@gmail.com>
2026-02-22 18:50:01 +0800

    Handling errors with Requests 1/2

---
+++ Itroduction to APIs in Python/Handling errors with Requests.py
@@ -0,0 +1,18 @@
+#Instructions 1/2
+# Import the exception class used to detect connection errors from the requests package, then use the imported class to intercept the error raised by the API request.
+
+import requests
+# Import the correct exception class
+from requests.exceptions import ConnectionError, HTTPError
+
+url ="http://wronghost:3000/albums"
+try: 
+    r = requests.get(url) 
+    print(r.status_code)
+# Use the imported class to intercept the connection error
+except ConnectionError as conn_err: 
+    print(f'Connection Error! {conn_err}.')
+
+    
+#Instructions 2/2
+# Import the exception class used to detect errors returned via the response status code, then enable the setting on the response object which will automatically raise an error when an unsuccessful status code value is received. Finally, intercept the imported exception to print an error.
